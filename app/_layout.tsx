a307180e9068d5f09931625b10a6b7895c83e13a
qkrekdls <gunmannduu@gmail.com>
2026-03-20 01:59:09 +0900

    feat: Info Card

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,45 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import {
+  Roboto_100Thin,
+  Roboto_300Light,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_600SemiBold,
+  Roboto_700Bold,
+  Roboto_900Black,
+  useFonts,
+} from "@expo-google-fonts/roboto";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+// 폰트 로딩 전까지 화면 유지
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const [loaded, error] = useFonts({
+    "Roboto-Thin": Roboto_100Thin,
+    "Roboto-Light": Roboto_300Light,
+    "Roboto-Regular": Roboto_400Regular,
+    "Roboto-Medium": Roboto_500Medium,
+    "Roboto-SemiBold": Roboto_600SemiBold,
+    "Roboto-Bold": Roboto_700Bold,
+    "Roboto-Black": Roboto_900Black,
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) return null;
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Stack screenOptions={{ headerShown: false }}>
+      {/* 모든 화면에서 상단바를 숨긴다 */}
+
+      {/* index.tsx가 첫 화면*/}
+      <Stack.Screen name="index" />
+    </Stack>
   );
 }
