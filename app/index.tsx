955d2d74ae167996b87faa7b07bad847f1d82cd5
seo1v <minseoeum14@gmail.com>
2026-03-29 14:18:06 +0900

    feat: 바이오, 게시물 사진 추가

--- app/index.tsx
+++ app/index.tsx
@@ -8,14 +8,36 @@ export default function ProfileScreen() {
           source={require('@/assets/images/profile.png')}
           style={styles.profileImage}
         />
+        <View style={styles.stats}>
+          <View style={styles.statBox}>
+            <Text style={styles.statNumber}>1</Text>
+            <Text>게시물</Text>
+          </View>
+          <View style={styles.statBox}>
+            <Text style={styles.statNumber}>100</Text>
+            <Text>팔로워</Text>
+          </View>
+          <View style={styles.statBox}>
+            <Text style={styles.statNumber}>100</Text>
+            <Text>팔로잉</Text>
+          </View>
+        </View>
       </View>
 
       <View style={styles.bioSection}>
         <Text style={styles.name}>엄민서</Text>
         <Text style={styles.bio}>숙명여자대학교 IT공학전공</Text>
-        <Text style={styles.bio}>iOS Developer</Text>
+      </View>
+
+      <View style={styles.grid}>
+        <Image
+        source={require('@/assets/images/post.png')}
+        style={styles.gridImage}
+        />
       </View>
     </ScrollView>
+
+    
   );
 }
 
@@ -29,6 +51,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     padding: 20,
     alignItems: 'center',
+    marginTop: 60,
   },
 
   profileImage: {
@@ -38,6 +61,19 @@ const styles = StyleSheet.create({
     marginRight: 20,
   },
 
+  stats: {
+    flexDirection: 'row',
+    flex: 1,
+    justifyContent: 'space-around',
+  },
+  statBox: {
+    alignItems: 'center',
+  },
+  statNumber: {
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  
   bioSection: {
     paddingHorizontal: 20,
     marginBottom: 20,
@@ -51,5 +87,16 @@ const styles = StyleSheet.create({
 
   bio: {
     fontSize: 14,
+  },
+
+  grid: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+  },
+  gridImage: {
+    width: '32%',
+    aspectRatio: 1,
+    marginBottom: 5,
   },
 });
