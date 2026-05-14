e08225af70d99ab0e06cadfd8e69c03f40f50338
yubin <iyubin284@gmail.com>
2026-05-14 18:27:53 +0900

    refactor:instragram components파일 구조 변경

--- app/instagram/index.tsx
+++ app/instagram/index.tsx
@@ -1,9 +1,10 @@
-import { Body } from '@/components/Body';
-import { Footer } from '@/components/Footer';
-import { Header } from '@/components/Header';
 import { StyleSheet, View } from 'react-native';
 
-export default function Week2Screen() {
+import { Body } from './components/Body';
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
+
+export default function InstagramScreen() {
   return (
     <View style={styles.screen}>
       <Header />
