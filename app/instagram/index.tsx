9d3d7842d3fa3e9ab98e2d0d889b91c91488b111
yubin <iyubin284@gmail.com>
2026-05-14 18:43:34 +0900

    refactor: instragram 파일 구조 개선

--- app/instagram/index.tsx
+++ app/instagram/index.tsx
@@ -1,8 +1,8 @@
 import { StyleSheet, View } from 'react-native';
 
-import { Body } from './components/Body';
-import { Footer } from './components/Footer';
-import { Header } from './components/Header';
+import { Body } from '@/src/features/instagram/components/Body';
+import { Footer } from '@/src/features/instagram/components/Footer';
+import { Header } from '@/src/features/instagram/components/Header';
 
 export default function InstagramScreen() {
   return (
